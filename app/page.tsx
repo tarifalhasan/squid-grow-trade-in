50a77e4fdbd576db59cbfac7db8d1093572d45ed
tarifalhasan <tarialhasan999@gmail.com>
2023-05-20 23:28:40 +0600

    add footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import FooterDesktop from '@/components/FooterDesktop';
 import Hero from '@/components/hero/Hero';
 import Image from 'next/image';
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <>
       <Hero />
+      <FooterDesktop />
     </>
   );
 }
